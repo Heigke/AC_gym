6210ed894178e8bd75db768de47a05346ce166c1
ZebraZwosh <elt14ebe@student.lu.se>
2020-04-20 14:37:20 +0200

    a

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,4 +1,4 @@
 from gym.envs.registration import register
-register(id=’CustomEnv-v0',
-    entry_point=’envs.AC_man_dir:AC_man’
+register(id='CustomEnv-v0',
+    entry_point='envs.AC_man_dir:AC_man'
 )

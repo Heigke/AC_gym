ddc99253715b2d5e340c0e76777958ff8b7a22ca
ZebraZwosh <elt14ebe@student.lu.se>
2020-04-30 09:35:57 +0200

    slow again latest that worked

--- envs/AC_man_dir/AC_man.py
+++ envs/AC_man_dir/AC_man.py
@@ -1,22 +1,23 @@
 import numpy as np
 from gym import spaces
-#import optimal_lqr_control
+import optimal_lqr_control
 #from stable_baselines.common.env_checker import check_env
 import gym
 import sys
+import matplotlib.pyplot as plt
 """
 Settings for linear quadratic regulator
 """
 
-A2 = np.array([[0,0,1],[0,1,0],[1,0,0]])
-B2 = np.array([[1,0,0],[0,1,0],[0,0,1]])
-C2 = np.array([[0,1,0],[1,1,0]])
+A2 = np.array([[0,0,1,0,0],[0,1,0,0,0],[1,0,0,0,0],[0,0,0,1,0],[0,0,0,0,1]])
+B2 = np.array([[1,0,0],[0,1,0],[0,0,1],[0,0,0],[0,0,0]])
+C2 = np.array([[0,1,0,0,0],[1,1,0,0,0],[0,0,0,1,0],[0,0,0,0,1]])
 Q2 = np.array([[1,0,0],[0,1,0],[0,0,1]])
 R2 = np.array([[1,0,0],[0,1,0],[0,0,1]])
 N2 = np.array([[0,0,0],[0,0,0],[0,0,0]])
-initial_value2 = np.array([[1],[1],[1]])
+initial_value2 = np.array([[1],[1],[1],[0],[1]])
 reset_rnd2 = True
-nonlin_lambda2 = lambda x: 0.2*np.sin(x)
+nonlin_lambda2 = lambda x: 0.0*np.sin(x)
 class Automatic_Control_Environment(gym.Env):
     """ ***A simple automatic control environment***
     by Niklas Kotarsky and Eric Bergvall
@@ -55,10 +56,12 @@ class Automatic_Control_Environment(gym.Env):
         self.action_space = spaces.Box(low=-high_vector_act, high=high_vector_act, dtype=np.float32)
         self.observation_space = spaces.Box(low=-high_vector_obs, high=high_vector_obs, dtype=np.float32)
         self.nonlin_term = nonlin_lambda
-
+        self.shifted = False
 
         self.rollout_steps = 19
-        #self.lqr_optimal = optimal_lqr_control.Lqr(A,B,Q,R,N,horizon)
+        self.mode = self.initial_value[-1]
+        self.start_shift = 0
+        self.unscaled_reward = 0
         
     def state_space_equation(self, action):
         noise = np.random.normal(0,1,self.state.shape)
@@ -73,21 +76,31 @@ class Automatic_Control_Environment(gym.Env):
         new_Y = self.C@self.state #+ noise
         return new_Y
     
-#    def opt_action(self):
-#        optimal_action = self.lqr_optimal.action(self.state)
-#        optimal_action = np.squeeze(optimal_action,axis=1)
-#        return optimal_action
+ #   def opt_action(self):
+ #       optimal_action = self.lqr_optimal.action(self.state)
+ #       optimal_action = np.squeeze(optimal_action,axis=1)
+ #       return optimal_action
 
     def step(self, action):
         action = np.expand_dims(action,axis=1)
         next_state = self.state_space_equation(action)
         done = self.done()
         self.state = next_state
+        if self.shifted == False:
+            if self.nbr_steps > 7 and self.nbr_steps < 15:
+                if np.random.uniform(0,10) > 5:
+                    self.state[-2] = np.random.choice([-1,0,1])
+                    self.mode = np.random.choice([1])
+                    self.state[-1] = self.mode
+                    self.shifted = True
+                    self.start_shift = self.nbr_steps
+                    #print("level shifted")
         self.action = action                                                                                                                             
         next_Y = self.new_obs()
         self.Y = next_Y
-        reward = self.reward()
         self.nbr_steps += 1
+        reward = self.reward()
+        
 
         next_Y = np.squeeze(next_Y,axis=1)
         next_Y = next_Y.astype('float32')
@@ -119,14 +132,16 @@ class Automatic_Control_Environment(gym.Env):
         return observable_check
 
     def reset(self):
+        self.shifted = False
         if self.reset_rnd:
             self.initial_value = np.random.uniform(-0.9,0.9,self.initial_value.shape)
-
+        
         self.state = self.initial_value
+        self.state[-2] = 0
+        self.state[-1] = 1
         self.Y = self.new_obs()
         self.action = self.initial_action
-        self.nbr_steps = 0                                                                                                                              
-        #self.lqr_optimal.reset()
+        self.nbr_steps = 0
         squeezed_obs = np.squeeze(self.Y,axis=1)
         return squeezed_obs
 
@@ -134,14 +149,36 @@ class Automatic_Control_Environment(gym.Env):
         return self.state
 
     def reward(self):
-        x = self.state
+        x = self.state[0:-2]
+        s = self.state[-2][0]*np.ones(x.shape)
         u = self.action
         x_T = np.transpose(x)
         u_T = np.transpose(u)
+        s_T = np.transpose(s)
         Q = self.Q
-        R = self.R
+        R_factor = 1
+        #if self.shifted == True:
+        #    if self.mode == 1:
+        #        R_factor = np.clip(np.power(10-(9/5)*(self.nbr_steps-self.start_shift),2),1,50)
+        #    elif self.mode == 2:
+        #        R_factor = np.clip(np.power(10-(9/5)*(self.nbr_steps-self.start_shift),2),1,50)
+        if self.shifted == True:
+            if self.mode == 1:
+                if (self.nbr_steps-self.start_shift) < 10:
+                    R_factor = 2
+                else:
+                    R_factor = 1
+            elif self.mode == 2:
+                if (self.nbr_steps-self.start_shift) < 10:
+                    R_factor = 10
+                else:
+                    R_factor = 1
+
+        R = R_factor * np.eye(self.R.shape[0])
         N = self.N
-        current_reward = x_T@Q@x+u_T@R@u+2*x_T@N@x
+        const_R = 1 * np.eye(self.R.shape[0])
+        self.unscaled_reward = -((x_T-s_T)@Q@(x-s)+u_T@const_R@u+2*x_T@N@x)[0][0]
+        current_reward = (x_T-s_T)@Q@(x-s)+u_T@R@u+2*x_T@N@x
         return -current_reward[0][0]
 
     def done(self):
@@ -177,10 +214,60 @@ if __name__ == "__main__":
     ac_env = Automatic_Control_Environment()
     print("obs space: "+str(ac_env.observation_space.shape))
     print("act space: "+str(ac_env.action_space.shape))
-    state = ac_env.reset()
-    optimal_action = ac_env.opt_action()
-    #action = np.array([0.1])
-    next_state, reward, done, _ = ac_env.step(optimal_action)
+    while True:
+        state = ac_env.reset()
+        print("Reset!")
+        a = []
+        ns = []
+        i_s = []
+        r = []
+        ur = []
+        action = np.array([0.1,0.1,0.1])
+        
+        for i in range(20):
+            
+            #optimal_action = ac_env.opt_action()
+            if ac_env.shifted == True:
+                action = 1*np.array([0.1,0.1,0.1])
+            else:
+                action = i*np.array([0.01,0.01,0.01])
+            next_state, reward, done, _ = ac_env.step(action)
+            print("Iteration:"+str(i))
+            print("State:"+str(next_state))
+            print("Reward"+str(reward))
+            a.append(action)
+            ns.append(next_state)
+            i_s.append(ac_env.state[:,0])
+            r.append(reward)
+            ur.append(ac_env.unscaled_reward)
+            
+        
+        plt.figure(figsize=(20,5))
+        plt.subplot(131)
+        plt.title('action')
+        plt.plot(a)
+        plt.show()
+        plt.figure(figsize=(20,5))
+        plt.subplot(131)
+        plt.title('next_state')
+        plt.plot(ns)
+        plt.show()
+        plt.figure(figsize=(20,5))
+        plt.subplot(131)
+        plt.title('inner_state')
+        plt.plot(i_s)
+        plt.show()
+        plt.figure(figsize=(20,5))
+        plt.subplot(131)
+        plt.title('reward')
+        plt.plot(r)
+        plt.show()
+        plt.figure(figsize=(20,5))
+        plt.subplot(131)
+        plt.title('unscaled reward')
+        plt.plot(ur)
+        plt.show()
+
     print("new state")
     print(next_state)
     print("rew")
